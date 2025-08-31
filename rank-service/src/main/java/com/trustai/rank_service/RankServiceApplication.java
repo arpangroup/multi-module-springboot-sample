c732114f72f66ace7ffc3bd05938df9064160699
arpan jana <arpangroup1@gmail.com>
2025-08-31 05:35:20 +0530

    remove redundant module

--- rank-service/src/main/java/com/trustai/rank_service/RankServiceApplication.java
+++ rank-service/src/main/java/com/trustai/rank_service/RankServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.trustai.*"})
+@ComponentScan(basePackages = {"com.trustai"})
 public class RankServiceApplication {
 
 	public static void main(String[] args) {
